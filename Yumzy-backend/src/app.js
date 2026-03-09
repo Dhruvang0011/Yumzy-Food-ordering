90f7eff793d289896997d7db525de32c46d824b7
Dhruvang0011 <dhchampaneriya@gmail.com>
2026-03-09 15:38:05 +0530

    c

--- Yumzy-backend/src/app.js
+++ Yumzy-backend/src/app.js
@@ -9,7 +9,7 @@ app.use(
   cors({
     origin: "https://yumzy-food-ordering.vercel.app",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
   })
 );
 app.options("*", cors());
