9e329f0a414ab277a29903fc868f31d80ee1727b
Dhruvang0011 <dhchampaneriya@gmail.com>
2026-03-09 15:23:50 +0530

    deployment

--- Yumzy-frontend/src/utils/axios.js
+++ Yumzy-frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:2411/api", // your backend
+  baseURL: "https://yumzy-food-ordering.onrender.com/api", // your backend
   withCredentials: true, // VERY IMPORTANT
 });
 
