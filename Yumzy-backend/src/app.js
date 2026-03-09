31712eafe21e131368e7bef759fcea5351fa17cc
Dhruvang0011 <dhchampaneriya@gmail.com>
2026-03-09 16:11:25 +0530

    c5

--- Yumzy-backend/src/app.js
+++ Yumzy-backend/src/app.js
@@ -10,6 +10,7 @@ app.use(
     origin: ["https://yumzy-food-ordering.vercel.app","http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
 
