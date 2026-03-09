eb06902bc30ed26a1f1c5c0d20d7f613c6b359b3
Dhruvang0011 <dhchampaneriya@gmail.com>
2026-03-09 16:01:21 +0530

    c3

--- Yumzy-backend/src/app.js
+++ Yumzy-backend/src/app.js
@@ -38,9 +38,11 @@ connectdb()
 .then(() => {
     console.log("✅ Database Connected Successfully...");
 
-    app.listen(2411 , ()=> {
-        console.log("🚀 Server running on port 2411");
-    })
+    const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 })
 .catch(()=> {
     console.error("❌ Database Connection Failed !!!");
